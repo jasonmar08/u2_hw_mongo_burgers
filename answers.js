aab9b527e0ddd67cca5be572a634d88be113998f
Jason Martinez <jasonmar08@gmail.com>
2022-07-20 00:36:13 -0400

    completed Bonus

--- answers.js
+++ answers.js
@@ -73,14 +73,50 @@ db.dropDatabase()
 // Bonus
 //recreate your burgers database and your burger collection
 //copy paste your insert burgers from above to reseed your database
-
+use burgers
+db.burger.insertMany([
+  {
+    protein: 'beef',
+    cheese: 'yes',
+    toppings: ['tomatoes', 'mayonnaise', 'onions', 'mustard']
+  },
+  {
+    protein: 'beef',
+    cheese: 'yes',
+    toppings: ['relish', 'peanut butter', 'maple syrup', 'ketchup', 'mustard']
+  },
+  {
+    protein: 'beef',
+    cheese: 'no',
+    toppings: ['onions', 'ramen', 'fried banana', 'tabasco', 'mushrooms']
+  },
+  {
+    protein: 'elk',
+    cheese: 'yes',
+    toppings: ['french fries', 'guacamole', 'capers', 'pickles', 'maple syrup']
+  },
+  {
+    protein: 'ostrich',
+    cheese: 'no',
+    toppings: [
+      'Worstershire sauce',
+      'pickled beets',
+      'mango',
+      'sriracha',
+      'mushrooms'
+    ]
+  }
+])
 // Change the name of the key cheese to 'pumpkinSpice'
-
+db.burger.updateMany({}, {$rename: {'cheese': 'pumpkinSpice'}})
 // find all the burgers with ketchup (or another topping you used at least once)
-
+db.burger.find({toppings: 'mushrooms'})
 // find all the burgers with pickles (or a topping you used more than once) and remove the pickles
-
+db.burger.updateMany({}, {$pull: {toppings: {$in: ['mushrooms']}}})
 // add a topping of 'eggs' to all the beef burgers
 //note since this db is 'reset' there should be no veggie burgers, all beef burgers should still be intact
+db.burger.updateMany({protein: {$eq: 'beef'}}, {$addToSet: {toppings: 'eggs'}})
 
+db.burger.updateMany({protein: 'beef'}, {$addToSet: {toppings: 'eggs'}})
 //Add a price to each burger, start with $5.00 for each burger
+db.burger.updateMany({}, {$set: {price: '$5.00'}})
